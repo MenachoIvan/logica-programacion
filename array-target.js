246d3fe257c64e441bf22cce15a6b70588e2e45a
Iván Menacho Domínguez <92464787+MenachoRBB@users.noreply.github.com>
2024-11-27 15:28:01 +0100

    fix: text formatted

--- array-target.js
+++ array-target.js
@@ -1,28 +1,28 @@
 /*
-Reto de @midudev
-Nivel: Fácil
+    Reto de @midudev
+    Nivel: Fácil
 
-Dado un array de enteros nums y un entero target,
-devuelve los indices de los dos numeros que suman el
-valor de target.
+    Dado un array de enteros nums y un entero target,
+    devuelve los indices de los dos numeros que suman el
+    valor de target.
 
-Puedes asumir que cada entrada tendra exactamente una
-solucion y no puedes usar el mismo elemento dos veces.
+    Puedes asumir que cada entrada tendra exactamente una
+    solucion y no puedes usar el mismo elemento dos veces.
 
-Puedes devolver la respuesta en cualquier orden.
+    Puedes devolver la respuesta en cualquier orden.
 
-Ejemplos:
+    Ejemplos:
 
-Entrada: nums = [2,7,11,15], target = 9
-Salida: [0,1]
-Explicacion: Dado que nums[0] + nums[1] == 9,
-devolvemos [0, 1].
+    Entrada: nums = [2,7,11,15], target = 9
+    Salida: [0,1]
+    Explicacion: Dado que nums[0] + nums[1] == 9,
+    devolvemos [0, 1].
 
-Entrada: nums = [3,2,4], target = 6
-Salida: [1,2] --> Si se comprueba el mismo numero puede ser [0,0]
+    Entrada: nums = [3,2,4], target = 6
+    Salida: [1,2] --> Si se comprueba el mismo numero puede ser [0,0]
 
-Entrada: nums = [3,3], target = 6
-Salida: [0,1] --> Si se comprueba el mismo numero puede ser [0,0]
+    Entrada: nums = [3,3], target = 6
+    Salida: [0,1] --> Si se comprueba el mismo numero puede ser [0,0]
 */ 
 
 function reto(nums, target){
--- subcadenas.js
+++ subcadenas.js
@@ -1,19 +1,22 @@
-/**
- * Dado un string 's', encuentra la longitud de la subcadena más larga que NO contenga carácteres repetidos.
- * 
- * Ejemplo 1:
- * 
- * Entrada: s= "abcabcbb"
- * Salida: 3
- * Explicación: La respuesta es "abc" con una longitud de 3
- * 
- * Ejemplo 2:
- * 
- * Entrada: s= "pwwkew"
- * Salida: 3
- * Explicación: La respuesta es "wke" con una longitud de 3
- * 
- * Ten en cuenta que la respuesta debe ser una subcadena, no una subsecuencia, "pwke" es una subsecuencia, pero no es una subcadena
+/*
+    Reto de @midudev
+    Nivel: Fácil
+
+    Dado un string 's', encuentra la longitud de la subcadena más larga que NO contenga carácteres repetidos.
+
+    Ejemplo 1:
+ 
+    Entrada: s= "abcabcbb"
+    Salida: 3
+    Explicación: La respuesta es "abc" con una longitud de 3
+ 
+    Ejemplo 2:
+  
+    Entrada: s= "pwwkew"
+    Salida: 3
+    Explicación: La respuesta es "wke" con una longitud de 3
+ 
+    Ten en cuenta que la respuesta debe ser una subcadena, no una subsecuencia, "pwke" es una subsecuencia, pero no es una subcadena
  */
 
 function subcadenaMasLarga(texto){
