75354172978a2792cc02f13ee73527e675466e60
Iván Menacho Domínguez <92464787+MenachoRBB@users.noreply.github.com>
2024-11-27 10:37:14 +0100

    feat: new challenge

---
+++ subcadenas.js
@@ -0,0 +1,22 @@
+/**
+ * Dado un string 's', encuentra la longitud de la subcadena más larga que NO contenga carácteres repetidos.
+ * 
+ * Ejemplo 1:
+ * 
+ * Entrada: s= "abcabcbb"
+ * Salida: 3
+ * Explicación: La respuesta es "abc" con una longitud de 3
+ * 
+ * Entrada: s= "pwwkew"
+ * Salida: 3
+ * Explicación: La respuesta es "wke" con una longitud de 3
+ * 
+ * Ten en cuenta que la respuesta debe ser una subcadena, no una subsecuencia, "pwke" es una subsecuencia, pero no es una subcadena-
+ */
+
+function subcadenaMasLarga(){
+
+
+
+    return 0;
+}
